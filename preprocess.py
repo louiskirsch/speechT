4c386e4d3f3767c9795506dc2048f4a6953c7abe
Louis Kirsch <louis@louiskirsch.com>
2017-01-03 07:32:31 +0100

    Added documentation for preprocessing

--- preprocess.py
+++ preprocess.py
@@ -25,7 +25,20 @@ import corpus
 import argparse
 
 
-def calc_spectrogram(audio_data, samplerate, n_mfcc=13, n_fft=400, hop_length=160):
+def calc_mfccs(audio_data, samplerate, n_mfcc=13, n_fft=400, hop_length=160):
+  """
+  Calculate mfcc coefficients from the given raw audio data
+
+  Args:
+    audio_data: numpyarray of raw audio wave
+    samplerate: the sample rate of the `audio_data`
+    n_mfcc: the number of coefficients to generate
+    n_fft: the window size of the fft
+    hop_length: the hop length for the window
+
+  Returns: the mfcc coefficients in the form [time, coefficients]
+
+  """
   mfcc = librosa.feature.mfcc(audio_data, sr=samplerate, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
 
   # TODO do we need to normalize to mean 0 and std 1 ?
@@ -34,6 +47,16 @@ def calc_spectrogram(audio_data, samplerate, n_mfcc=13, n_fft=400, hop_length=16
 
 
 def iglob_recursive(directory, file_pattern):
+  """
+  Recursively search for `file_pattern` in `directory`
+
+  Args:
+    directory: the directory to search in
+    file_pattern: the file pattern to match (wildcard compatible)
+
+  Returns: iterator for found files
+
+  """
   for root, dir_names, file_names in os.walk(directory):
     for filename in fnmatch.filter(file_names, file_pattern):
       yield os.path.join(root, filename)
@@ -41,9 +64,15 @@ def iglob_recursive(directory, file_pattern):
 
 class SpeechCorpusReader:
   """
-  Reads and transforms the speech corpus to be used by the NN
+  Reads preprocessed speech corpus to be used by the NN
   """
   def __init__(self, data_directory):
+    """
+    Create SpeechCorpusReader and read samples from `data_directory`
+
+    Args:
+      data_directory: the directory to use
+    """
     self._data_directory = data_directory
     self._transcript_dict_cache = None
 
@@ -57,10 +86,11 @@ class SpeechCorpusReader:
   def _get_transcript_entries(transcript_directory):
     """
     Iterate over all transcript lines and yield splitted entries
-    Args:
-      transcript_files: transcript files to iterate over
 
-    Returns: Iterator for all splitted entries
+    Args:
+      transcript_directory: open all transcript files in this directory and extract their contents
+
+    Returns: Iterator for all entries in the form (id, sentence)
 
     """
     transcript_files = iglob_recursive(transcript_directory, '*.trans.txt')
@@ -78,7 +108,8 @@ class SpeechCorpusReader:
   def _build_transcript(self):
     """
     Builds a transcript from transcript files, mapping from audio-id to a list of vocabulary ids
-    :return: the created transcript
+
+    Returns: the created transcript
     """
 
     # Create the transcript dictionary
@@ -91,8 +122,9 @@ class SpeechCorpusReader:
   def generate_samples(self, directory):
     """
     Generates samples from the given directory
+
     :param directory: the sub-directory of the initial data directory to sample from
-    :return: generator with (audio_fragments: ndarray, transcript: list(int)) tuples
+    :return: generator with (audio_id: string, audio_fragments: ndarray, transcript: list(int)) tuples
     """
     audio_files = list(iglob_recursive(self._data_directory + '/' + directory, '*.flac'))
 
@@ -100,7 +132,7 @@ class SpeechCorpusReader:
 
     for audio_file in audio_files:
       audio_data, samplerate = librosa.load(audio_file)
-      audio_fragments = calc_spectrogram(audio_data, samplerate)
+      audio_fragments = calc_mfccs(audio_data, samplerate)
 
       file_name = os.path.basename(audio_file)
       audio_id = os.path.splitext(file_name)[0]
@@ -110,6 +142,13 @@ class SpeechCorpusReader:
       yield audio_id, audio_fragments, transcript
 
   def store_samples(self, directory):
+    """
+    Read audio files from `directory` and store the preprocessed version in preprocessed/`directory`
+
+    Args:
+      directory: the sub-directory to read from
+
+    """
 
     out_directory = self._data_directory + '/preprocessed/' + directory
     if not os.path.exists(out_directory):
@@ -119,6 +158,18 @@ class SpeechCorpusReader:
       np.savez(out_directory + '/' + audio_id, audio_fragments=audio_fragments, transcript=transcript)
 
   def load_samples(self, directory, max_size=False, loop_infinitely=False, limit_count=0):
+    """
+    Load the preprocessed samples from `directory` and return an iterator
+
+    Args:
+      directory: the sub-directory to use
+      max_size: the maximum audio time length, all others are discarded (default: False)
+      loop_infinitely: after one pass, shuffle and pass again (default: False)
+      limit_count: maximum number of samples to use, 0 equals unlimited (default: 0)
+
+    Returns: iterator for samples (audio_data, transcript)
+
+    """
 
     load_directory = self._data_directory + '/preprocessed/' + directory
 
