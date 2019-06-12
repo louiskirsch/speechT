e8232e227ad6003a39b451d185fff6f0d7186a29
longisland04 <654749513@qq.com>
2019-06-13 01:24:31 +0800

    first version

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 setup(
   name='speechT',
